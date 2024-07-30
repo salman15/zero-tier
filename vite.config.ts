b4ade31e67ff8ae99e79f66fcdacdf1e0d0742f5
Salman <salmannova@gmail.com>
2024-07-30 14:20:05 +0200

    feat(tailwind): Added basic setup of twin + tailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020",
+    },
+  },
+  esbuild: {
+    // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
+    logOverride: { "this-is-undefined-in-esm": "silent" },
+  },
+
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          "babel-plugin-macros",
+          [
+            "@emotion/babel-plugin-jsx-pragmatic",
+            {
+              export: "jsx",
+              import: "__cssprop",
+              module: "@emotion/react",
+            },
+          ],
+          [
+            "@babel/plugin-transform-react-jsx",
+            { pragma: "__cssprop" },
+            "twin.macro",
+          ],
+        ],
+      },
+    }),
+  ],
+  define: {
+    "process.env": {
+      ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+    },
+  },
+});
